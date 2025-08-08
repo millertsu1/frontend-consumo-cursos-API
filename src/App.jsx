3737a6df9b47244f6dd6650e081f542a2071a182
Miller Ladino Osorio <MLADINO@NUTRABIOTICS.LOCAL>
2025-08-07 19:46:27 -0500

    se ajusto la tarjeta del curso

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 // src/App.jsx
 
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() { // La función del componente
+function App() {
+  // La función del componente
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => { // La llamada a useEffect con la función a ejecutar
+  useEffect(() => {
+    // La llamada a useEffect con la función a ejecutar
     const fetchItems = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/api/cursos/');
+        const response = await axios.get("http://127.0.0.1:8000/api/cursos/");
         setItems(response.data);
       } catch (err) {
         setError(err);
@@ -28,11 +30,16 @@ function App() { // La función del componente
   return (
     <div className="App">
       <h1>Lista de cursos</h1>
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>{item.nombre}</li>
+      
+        {items.map((item) => (
+          <div key={item.id}>
+            <h2 >{item.nombre}</h2>
+            <p>{item.descripcion}</p>
+            <small>Autor: {item.creador}</small>
+            <p>{item.duracion} minutos</p>
+          </div>
         ))}
-      </ul>
+      
     </div>
   );
 }
